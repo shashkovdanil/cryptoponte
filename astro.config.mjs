a9a40c325e6d867fb20938af57075b80c30383ef
Danil Shashkov <shashkovdanil@gmail.com>
2023-10-03 18:20:12 +0100

    vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
-import tailwind from '@astrojs/tailwind'
-import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-})
+  output: "server",
+  adapter: vercel()
+});
